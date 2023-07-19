c1ce470384c6e06d9f4c8dcb04501157e42cc547
David Arenas <davejs136@gmail.com>
2023-07-18 21:03:46 -0400

    refactor: extract deepmerge to utils folder

---
+++ packages/vue-email/src/config.ts
@@ -0,0 +1,37 @@
+import { deepmerge } from './utils/deepmerge'
+import type { Options } from './types'
+
+/**
+ * Merges the default configuration options with the provided.
+ *
+ * @param options
+ */
+export function createInitConfig(options: Options): Options {
+  const config = deepmerge<Options>({
+    dir: options.dir,
+    verbose: false,
+    output: {
+      auto: true,
+      dir: `${options.dir}/.vuemail`,
+    },
+  }, options)
+
+  return config
+}
+
+/**
+ * Validates the configuration object.
+ *
+ * @param config
+ */
+export function validateConfig(config: Options): void {
+  // TODO: add a good console logging
+  console.log('Validating configuration...')
+
+  const required: (keyof Options)[] = ['dir']
+  const missing = required.filter((r) => !config[r])
+
+  if (missing.length) {
+    throw new Error('The following configuration options are missing: ' + missing.join(', '))
+  }
+}
